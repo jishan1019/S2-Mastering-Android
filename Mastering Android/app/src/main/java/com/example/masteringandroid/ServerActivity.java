6368a7c6a458dbfc722d208138ddc2c48b9dd5ea
jishan <imranislamjishan80@gmail.com>
2024-06-19 17:30:02 +0600

    array fetch and show

--- Mastering Android/app/src/main/java/com/example/masteringandroid/ServerActivity.java
+++ Mastering Android/app/src/main/java/com/example/masteringandroid/ServerActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -18,49 +24,38 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ServerActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-    TextView textView;
+public class ServerActivity extends AppCompatActivity {
 
     String URL = "https://jsonplaceholder.typicode.com/users";
 
+    ProgressBar progressBar;
+    ListView listView;
+
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+    HashMap<String, String> hashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
 
-        textView = findViewById(R.id.tvDisplay);
-
-
-//        vollyReq();
+        progressBar = findViewById(R.id.progressBar);
+        listView = findViewById(R.id.listview);
 
         vollyArrReq();
 
 
-    }
 
-    private void vollyReq(){
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        textView.setText("Response is: " + response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!" + error);
-            }
-        });
-
-        queue.add(stringRequest);
     }
 
     private void vollyArrReq(){
         RequestQueue queue = Volley.newRequestQueue(this);
+
+        progressBar.setVisibility(View.VISIBLE);
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
             @Override
@@ -68,14 +63,24 @@ public class ServerActivity extends AppCompatActivity {
 
                try {
 
+                   progressBar.setVisibility(View.GONE);
+
                    for (int i =0 ; i< response.length(); i++){
                        JSONObject jsonObject = response.getJSONObject(i);
 
+                       hashMap =  new HashMap<>();
+                       hashMap.put("name", jsonObject.getString("name"));
+                       hashMap.put("email", jsonObject.getString("email"));
+                       hashMap.put("address", jsonObject.getString("address"));
+                       arrayList.add(hashMap);
 
                    }
 
-               }catch (JSONException e){
+                   MyAdaptar myAdaptar = new MyAdaptar();
+                   listView.setAdapter(myAdaptar);
 
+               }catch (JSONException e){
+                   progressBar.setVisibility(View.GONE);
                }
 
 
@@ -89,6 +94,51 @@ public class ServerActivity extends AppCompatActivity {
 
         queue.add(jsonArrayRequest);
 
+    }
+
+
+    private class MyAdaptar extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater layoutInflater = getLayoutInflater();
+            View view = layoutInflater.inflate(R.layout.server_list_item , null);
+
+
+            TextView tvName = view.findViewById(R.id.tvName);
+            TextView tvEmail = view.findViewById(R.id.tvEmail);
+            TextView tvAddress = view.findViewById(R.id.tvAddress);
+
+
+            HashMap<String, String> hashmap = arrayList.get(position);
+
+            String name = hashmap.get("name");
+            String email = hashmap.get("email");
+            String address = hashmap.get("address");
+
+            tvName.setText(name);
+            tvEmail.setText(email);
+            tvAddress.setText(address);
+
+
+            return view;
+        }
     }
 
 
