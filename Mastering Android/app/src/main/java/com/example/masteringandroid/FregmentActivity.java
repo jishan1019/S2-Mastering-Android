fabd268efbd99bf99dbe0ecadf4af963923c5a01
jishan <imranislamjishan80@gmail.com>
2024-06-21 00:58:06 +0600

    practice fregment and tab layout

--- Mastering Android/app/src/main/java/com/example/masteringandroid/FregmentActivity.java
+++ Mastering Android/app/src/main/java/com/example/masteringandroid/FregmentActivity.java
@@ -7,35 +7,56 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class FregmentActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fregment);
 
+        tabLayout = findViewById(R.id.tabLayout);
 
-        findViewById(R.id.btn1fregment).setOnClickListener(new View.OnClickListener() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, new FirstFragment());
+        fragmentTransaction.commit();
+
+
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.add(R.id.frameLayout, new FirstFragment());
-                fragmentTransaction.commit();
+            public void onTabSelected(TabLayout.Tab tab) {
+                int tabPosition = tab.getPosition();
+
+                if(tabPosition == 0){
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new FirstFragment());
+                    fragmentTransaction.commit();
+                }else if (tabPosition == 1){
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new SecondFragment());
+                    fragmentTransaction.commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
-
-
-        findViewById(R.id.btn2fregment).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.add(R.id.frameLayout, new SecondFragment());
-                fragmentTransaction.commit();
-            }
-        });
 
 
 
